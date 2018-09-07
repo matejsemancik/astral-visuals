45321a44d95683228edf17d9aec2513ca9f73129
matsem <matej.semancik@gmail.com>
2018-09-07 19:19:40 +0200

    implement gaining and frequency range drawing

--- src/main/kotlin/tools/audio/AudioProcessor.kt
+++ src/main/kotlin/tools/audio/AudioProcessor.kt
@@ -2,19 +2,16 @@ package tools.audio
 
 import ddf.minim.AudioListener
 import ddf.minim.Minim
-import ddf.minim.analysis.BeatDetect
 import ddf.minim.analysis.FFT
 import processing.core.PApplet
 
 class AudioProcessor constructor(private val sketch: PApplet) : AudioListener {
 
     override fun samples(p0: FloatArray?) {
-        beatDetect.detect(audioInput.mix)
         fft.forward(audioInput.mix)
     }
 
     override fun samples(p0: FloatArray?, p1: FloatArray?) {
-        beatDetect.detect(audioInput.mix)
         fft.forward(audioInput.mix)
     }
 
@@ -23,27 +20,47 @@ class AudioProcessor constructor(private val sketch: PApplet) : AudioListener {
         addListener(this@AudioProcessor)
     }
     val fft = FFT(audioInput.bufferSize(), audioInput.sampleRate()).apply {
-        logAverages(22, 1)
+        logAverages(22, 3)
     }
 
-    val beatDetect = BeatDetect(audioInput.bufferSize(), audioInput.sampleRate()).apply {
-        setSensitivity(150)
+    var gain = 1f
+    private val ranges = mutableListOf<ClosedFloatingPointRange<Float>>()
+
+    fun addRange(range: ClosedFloatingPointRange<Float>) {
+        ranges.add(range)
+    }
+
+    fun getRange(range: ClosedFloatingPointRange<Float>): Float {
+        return fft.calcAvg(range.start, range.endInclusive) * gain
     }
 
     fun drawDebug() {
-        val rectHeight = 8 // px
+        val rectHeight = 12 // px
 
         sketch.pushMatrix()
         sketch.translate(12f, 12f)
-        for (i in 0 until fft.avgSize()) {
-            // Draw frequency band
-            sketch.fill(110f, 225f, 255f)
-            sketch.rect(0f, i.toFloat() * rectHeight, fft.getAvg(i), rectHeight.toFloat())
 
-            // Draw band frequency value
-            sketch.fill(255f, 255f, 255f)
-            sketch.textSize(10f)
-            sketch.text("${fft.getAverageCenterFrequency(i)} Hz", 0f, i.toFloat() * rectHeight + rectHeight)
+        if (ranges.isNotEmpty()) {
+            for (i in 0 until ranges.size) {
+                val range = ranges[i]
+                sketch.fill(110f, 225f, 255f)
+                sketch.rect(0f, i.toFloat() * rectHeight, getRange(range), rectHeight.toFloat())
+
+                // Draw band frequency value
+                sketch.fill(255f, 255f, 255f)
+                sketch.textSize(10f)
+                sketch.text("${range.start} - ${range.endInclusive} Hz", 0f, i.toFloat() * rectHeight + rectHeight)
+            }
+        } else {
+            for (i in 0 until fft.avgSize()) {
+                sketch.fill(110f, 225f, 255f)
+                sketch.rect(0f, i.toFloat() * rectHeight, fft.getAvg(i) * gain, rectHeight.toFloat())
+
+                // Draw band frequency value
+                sketch.fill(255f, 255f, 255f)
+                sketch.textSize(10f)
+                sketch.text("${fft.getAverageCenterFrequency(i)} Hz", 0f, i.toFloat() * rectHeight + rectHeight)
+            }
         }
 
         sketch.popMatrix()
