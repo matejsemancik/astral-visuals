e2ebe0e7f0bb79cc46691b511fa68a1bb6aa9cd3
matsem <matej.semancik@gmail.com>
2018-12-26 01:29:29 +0100

    Add Box2D lib with FFT physics

--- src/main/kotlin/sketches/boxes/Box.kt
+++ src/main/kotlin/sketches/boxes/Box.kt
@@ -1,11 +1,55 @@
 package sketches.boxes
 
+import org.jbox2d.collision.shapes.PolygonShape
+import org.jbox2d.common.Vec2
+import org.jbox2d.dynamics.*
 import processing.core.PApplet
+import shiffman.box2d.Box2DProcessing
 
-class Box(val sketch: PApplet, val x: Float, val y: Float) {
+class Box(private val sketch: PApplet, private val box2d: Box2DProcessing, val x: Float, val y: Float) {
     var size = 25f
     var color = 0
     var strokeWeight = 3f
+
+    val body: Body
+    val fixture: Fixture
+
+    init {
+        val bd = BodyDef().apply {
+            position.set(box2d.coordPixelsToWorld(x, y))
+            type = BodyType.DYNAMIC
+        }
+
+        body = box2d.createBody(bd)
+        val shape = PolygonShape()
+        shape.setAsBox(
+                box2d.scalarPixelsToWorld(size / 2f),
+                box2d.scalarPixelsToWorld(size / 2f)
+        )
+
+        val fd = FixtureDef().apply {
+            this.shape = shape
+            density = 0.5f
+            friction = 0.0f
+            restitution = 0.3f
+        }
+
+        fixture = body.createFixture(fd)
+    }
+
+    fun applyForce(force: Vec2) {
+        body.applyForce(force, body.worldCenter)
+    }
+
+    fun attract(x: Float, y: Float, force: Float) {
+        val targetVec = box2d.coordPixelsToWorld(x, y)
+        val bodyVec = body.worldCenter
+
+        targetVec.subLocal(bodyVec)
+        targetVec.normalize()
+        targetVec.mulLocal(force)
+        body.applyForce(targetVec, bodyVec)
+    }
 
     fun draw() {
         with(sketch) {
@@ -14,7 +58,10 @@ class Box(val sketch: PApplet, val x: Float, val y: Float) {
             strokeWeight(strokeWeight)
 
             pushMatrix()
-            translate(x, y)
+            val coords = box2d.getBodyPixelCoord(body)
+            val angle = -body.angle
+            translate(coords.x, coords.y)
+            rotateZ(angle)
             sketch.box(size)
             popMatrix()
         }
--- src/main/kotlin/sketches/boxes/BoxesSketch.kt
+++ src/main/kotlin/sketches/boxes/BoxesSketch.kt
@@ -1,5 +1,8 @@
 package sketches.boxes
 
+import org.jbox2d.common.Vec2
+import processing.core.PConstants
+import shiffman.box2d.Box2DProcessing
 import sketches.BaseSketch
 import sketches.SketchLoader
 import tools.audio.AudioProcessor
@@ -14,29 +17,61 @@ class BoxesSketch(
         audioProcessor,
         galaxy
 ) {
+    lateinit var boundary: Boundary
     val boxes = arrayListOf<Box>()
-
-    override fun setup() {
-
-    }
+    val box2d = Box2DProcessing(sketch)
 
     override fun onBecameActive() {
+        rectMode(PConstants.CORNER)
+    }
 
+    override fun setup() {
+        box2d.createWorld(Vec2(0f, -9.81f * box2d.scaleFactor))
+        boundary = Boundary(sketch, box2d)
     }
 
     override fun draw() {
+        box2d.step()
+
+        if (sketch.keyPressed && sketch.key == 'a') {
+            boxes.add(
+                    Box(sketch, box2d, mouseX.toFloat(), mouseY.toFloat()).apply {
+                        color = accentColor
+                        size = sketch.random(20f, 30f)
+                    }
+            )
+        }
+
         background(bgColor)
 
+        pushMatrix()
+        noStroke()
+        fill(fgColor)
+        val w = width.toFloat() / audioProcessor.fft.avgSize()
+        for (i in 0 until audioProcessor.fft.avgSize()) {
+            val start = i * w
+            val end = (i + 1) * w
+
+            rect(i * w, height.toFloat(), w, -audioProcessor.fft.getAvg(i))
+            boxes
+                    .filter {
+                        val pixelCoords = box2d.coordWorldToPixels(it.body.worldCenter)
+                        return@filter (start..end).contains(pixelCoords.x)
+                    }
+                    .forEach {
+                        val pixelCoords = box2d.coordWorldToPixels(it.body.worldCenter)
+                        it.attract(pixelCoords.x, height - audioProcessor.fft.getAvg(i), 5000f)
+//                        it.applyForce(Vec2(0f, audioProcessor.fft.getAvg(i)))
+                    }
+        }
+        popMatrix()
+
         boxes.forEach {
+            if (mousePressed) {
+                it.attract(mouseX.toFloat(), mouseY.toFloat(), 1000f)
+            }
+
             it.draw()
         }
-    }
-
-    override fun mousePressed() {
-        boxes.add(
-                Box(sketch, mouseX.toFloat(), mouseY.toFloat()).apply {
-                    color = accentColor
-                }
-        )
     }
 }
