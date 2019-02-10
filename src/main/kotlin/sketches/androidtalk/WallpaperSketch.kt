b255bb4702c9fcaa7cf2ff4bac9327e00e1dbd68
matsem <matej.semancik@gmail.com>
2019-02-10 18:04:50 +0100

    Draw dots with perlin noise

--- src/main/kotlin/sketches/androidtalk/WallpaperSketch.kt
+++ src/main/kotlin/sketches/androidtalk/WallpaperSketch.kt
@@ -2,6 +2,7 @@ package sketches.androidtalk
 
 import processing.core.PApplet
 import processing.core.PConstants
+import processing.core.PVector
 
 class WallpaperSketch : PApplet() {
 
@@ -10,12 +11,15 @@ class WallpaperSketch : PApplet() {
 
     var numX = 20
     var numY = 30
-    var paddingVerticalPx = 40
-    var paddingHorizontalPx = 40
-    var dotSize = 5f
+    var paddingVerticalPx = 80
+    var paddingHorizontalPx = 80
+    var dotSize = 8f
+    val lineWeight = 2f
+    var dotPositions = Array(numX) { Array(numY) { PVector() } }
+    var fingerVector: PVector? = null
 
     override fun settings() {
-        size(480, 720, PConstants.P2D)
+        size(480, 720, PConstants.P3D)
 //        fullScreen(PConstants.P2D)
 //        fullScreen(PConstants.P3D)
     }
@@ -27,22 +31,72 @@ class WallpaperSketch : PApplet() {
 
         bgColor = color(0, 0, 15) // Dark gray
         fgColor = color(33, 100, 100) // Orange
+
+        for (x in 0 until numX) {
+            for (y in 0 until numY) {
+                val posX = PApplet.map(
+                        x.toFloat(),
+                        0f,
+                        numX.toFloat() - 1,
+                        -width / 2f + paddingVerticalPx,
+                        width / 2f - paddingVerticalPx
+                )
+                val posY = PApplet.map(
+                        y.toFloat(),
+                        0f,
+                        numY.toFloat() - 1,
+                        -height / 2f + paddingHorizontalPx,
+                        height / 2f - paddingHorizontalPx
+                )
+
+                dotPositions[x][y] = PVector(posX, posY, 0f)
+            }
+        }
     }
 
     override fun draw() {
         background(bgColor)
-        noStroke()
-        fill(fgColor)
+
+        translate(width / 2f, height / 2f)
 
         for (x in 0 until numX) {
             for (y in 0 until numY) {
+                val noise = noise(x / 10f, y / 10f + millis() / 1000f) * 100f
+                val position = dotPositions[x][y]
+
+                // Draw a line
+                strokeWeight(lineWeight)
+                stroke(fgColor)
+                line(
+                        position.x,
+                        position.y,
+                        0f,
+                        position.x,
+                        position.y,
+                        noise
+                )
+
+                // Draw dot
+                pushMatrix()
+                noStroke()
+                fill(fgColor)
+                translate(0f, 0f, noise)
                 ellipse(
-                        PApplet.map(x.toFloat(), 0f, numX.toFloat() - 1, 0f + paddingVerticalPx, width.toFloat() - paddingVerticalPx),
-                        PApplet.map(y.toFloat(), 0f, numY.toFloat() - 1, 0f + paddingVerticalPx, height.toFloat() - paddingVerticalPx),
+                        dotPositions[x][y].x,
+                        dotPositions[x][y].y,
                         dotSize,
                         dotSize
                 )
+                popMatrix()
             }
         }
+    }
+
+    override fun mouseDragged() {
+        fingerVector = PVector(mouseX.toFloat(), mouseY.toFloat())
+    }
+
+    override fun mouseReleased() {
+        fingerVector = null
     }
 }
