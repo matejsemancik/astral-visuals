4a0b8373f6972310167ac63fd243dd95b9a47508
matsem <matej.semancik@gmail.com>
2019-03-07 22:59:30 +0100

    Move App into separate file

--- src/main/kotlin/dev/matsem/astral/Main.kt
+++ src/main/kotlin/dev/matsem/astral/Main.kt
@@ -1,11 +1,7 @@
 package dev.matsem.astral
 
 import dev.matsem.astral.di.appModule
-import dev.matsem.astral.sketches.SketchLoader
-import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
-import org.koin.core.inject
-import processing.core.PApplet
 
 fun main(args: Array<String>) {
     startKoin {
@@ -13,15 +9,5 @@ fun main(args: Array<String>) {
         modules(appModule)
     }
 
-    App().start()
-}
-
-class App : KoinComponent {
-
-    private val sketchLoader: SketchLoader by inject()
-
-    fun start() {
-        val processingArgs = arrayOf("Sketch")
-        PApplet.runSketch(processingArgs, sketchLoader)
-    }
+    App().run()
 }
