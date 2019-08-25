25cdcb8e86e8f5b7687c2fa7b2a4fc1e0a1c85ea
matsem <matej.semancik@gmail.com>
2019-08-25 20:54:03 +0200

    Flash stars to music

--- src/main/kotlin/dev/matsem/astral/sketches/starfield/StarfieldSketch.kt
+++ src/main/kotlin/dev/matsem/astral/sketches/starfield/StarfieldSketch.kt
@@ -2,7 +2,9 @@ package dev.matsem.astral.sketches.starfield
 
 import dev.matsem.astral.tools.audio.AudioProcessor
 import dev.matsem.astral.tools.audio.beatcounter.BeatCounter
+import dev.matsem.astral.tools.extensions.constrain
 import dev.matsem.astral.tools.extensions.midiRange
+import dev.matsem.astral.tools.extensions.remap
 import dev.matsem.astral.tools.extensions.translateCenter
 import dev.matsem.astral.tools.kontrol.KontrolF1
 import org.koin.core.KoinComponent
@@ -18,7 +20,9 @@ class StarfieldSketch : PApplet(), KoinComponent {
             val vec: PVector,
             val diameter: Float,
             val ySpeed: Float = 0f,
-            val zSpeed: Float = 0f
+            val zSpeed: Float = 0f,
+            val randomFactor: Float = 0f,
+            var rotationExtra: Float = 0f
     )
 
     private val audioProcessor: AudioProcessor by inject()
@@ -37,7 +41,7 @@ class StarfieldSketch : PApplet(), KoinComponent {
         colorMode(PConstants.HSB, 360f, 100f, 100f)
 
         // Create galaxy from image
-        val galaxyImage: PImage = loadImage("images/galaxy-transparent-bw.png").apply { resize(640, 640) }
+        val galaxyImage: PImage = loadImage("images/galaxy-transparent-bw.png").apply { resize(720, 720) }
         galaxyImage.loadPixels()
         var pixelBrightness: Float
         for (x in 0 until galaxyImage.width step 2) {
@@ -51,7 +55,8 @@ class StarfieldSketch : PApplet(), KoinComponent {
                                     y.toFloat() - galaxyImage.height / 2f
                             ),
                             diameter = random(1f, 4f),
-                            ySpeed = random(0.00002f, 0.000025f)
+                            ySpeed = random(0.00006f, 0.0001f),
+                            randomFactor = random(0.01f, 0.1f)
                     )
                 }
             }
@@ -63,7 +68,8 @@ class StarfieldSketch : PApplet(), KoinComponent {
                     vec = PVector.random3D().mult(random(0f, 2500f)),
                     diameter = if (random(1f) > 0.99f) random(6f, 10f) else random(1f, 4f),
                     ySpeed = random(0.00002f, 0.00005f),
-                    zSpeed = random(-0.00001f, 0.00001f)
+                    zSpeed = random(-0.00001f, 0.00001f),
+                    randomFactor = if (random(1f) > 0.50f) random(0.2f, 1f) else 0f
             )
         }
     }
@@ -79,8 +85,9 @@ class StarfieldSketch : PApplet(), KoinComponent {
         galaxy.forEach {
             pushMatrix()
             translateCenter()
+            it.rotationExtra += audioProcessor.getRange(20f..100f).remap(0f, 100f, 0f, 0.02f) * it.randomFactor
             rotateX(kontrol.knob1.midiRange(PConstants.PI / 4f, -PConstants.PI / 4f))
-            rotateY(millis() * it.ySpeed)
+            rotateY(millis() * it.ySpeed + it.rotationExtra)
             rotateZ(millis() * it.zSpeed)
 
             strokeWeight(it.diameter)
@@ -89,16 +96,24 @@ class StarfieldSketch : PApplet(), KoinComponent {
         }
 
         // Stars
-        starField.forEach {
-            pushMatrix()
-            translateCenter()
-            rotateY(millis() * it.ySpeed)
-            rotateZ(millis() * it.zSpeed)
+        starField
+                .shuffled()
+                .take(audioProcessor
+                        .getRange(20f..60f)
+                        .remap(0f, 400f, starField.size.toFloat(), starField.size.toFloat() / 2f).toInt()
+                        .constrain(high = starField.size - 1)
+                )
+                .forEach {
+                    pushMatrix()
+                    translateCenter()
+                    it.rotationExtra += audioProcessor.getRange(2500f..16000f).remap(0f, 100f, 0f, 0.2f) * it.randomFactor
+                    rotateY(millis() * it.ySpeed + it.rotationExtra)
+                    rotateZ(millis() * it.zSpeed)
 
-            strokeWeight(it.diameter)
-            point(it.vec.x, it.vec.y, it.vec.z)
-            popMatrix()
-        }
+                    strokeWeight(it.diameter)
+                    point(it.vec.x, it.vec.y, it.vec.z)
+                    popMatrix()
+                }
 
         // Black hole
         pushMatrix()
