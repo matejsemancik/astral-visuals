9efb7eb7fb96e5ff4aa9d2f587319db72e281dbe
matsem <matej.semancik@gmail.com>
2019-08-31 15:58:04 +0200

    Add angularTime extensions

--- src/main/kotlin/dev/matsem/astral/tools/extensions/PAppletExtensions.kt
+++ src/main/kotlin/dev/matsem/astral/tools/extensions/PAppletExtensions.kt
@@ -1,6 +1,7 @@
 package dev.matsem.astral.tools.extensions
 
 import processing.core.PApplet
+import processing.core.PConstants
 
 fun PApplet.centerX() = this.width / 2f
 
@@ -12,3 +13,7 @@ fun PApplet.centerY() = this.height / 2f
 fun PApplet.saw(fHz: Float): Float = (millis() % (1000f * 1 / fHz)) / (1000f * 1 / fHz)
 
 fun PApplet.translateCenter() = translate(centerX(), centerY())
+
+fun PApplet.angularTimeS(periodSeconds: Float) = millis() / 1000f * PConstants.TWO_PI / periodSeconds
+
+fun PApplet.angularTimeHz(hz: Float) = millis() / 1000f * PConstants.TWO_PI / (1f / hz)
