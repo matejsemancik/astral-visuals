ec62a37eb64d018764180fd506cbfe77da9c6c20
matsem <matej.semancik@gmail.com>
2019-09-29 15:11:43 +0200

    Change music file in config

--- src/main/kotlin/dev/matsem/astral/Config.kt
+++ src/main/kotlin/dev/matsem/astral/Config.kt
@@ -14,7 +14,7 @@ object Config {
 
     object VideoExport {
         const val IS_IN_RENDER_MODE = false
-        const val AUDIO_FILE_PATH = "music/seba-move-on.wav"
+        const val AUDIO_FILE_PATH = "music/seba2.wav"
         const val SEP = "|"
         const val MOVIE_FPS = 25f
         const val FRAME_DURATION = 1f / MOVIE_FPS
