b5978030e66d2d1683c03a24302da296c6713403
matsem <matej.semancik@gmail.com>
2019-10-14 20:32:17 +0200

    Pimp up video sketch with new video, astral logo and touchosc controller

--- src/main/kotlin/dev/matsem/astral/sketches/video/VideoSketch.kt
+++ src/main/kotlin/dev/matsem/astral/sketches/video/VideoSketch.kt
@@ -3,33 +3,82 @@ package dev.matsem.astral.sketches.video
 import dev.matsem.astral.sketches.BaseSketch
 import dev.matsem.astral.sketches.SketchLoader
 import dev.matsem.astral.tools.audio.AudioProcessor
-import dev.matsem.astral.tools.extensions.centerX
-import dev.matsem.astral.tools.extensions.centerY
+import dev.matsem.astral.tools.extensions.*
+import dev.matsem.astral.tools.galaxy.Galaxy
 import org.koin.core.inject
 import processing.video.Movie
+import kotlin.random.Random
 
 class VideoSketch : BaseSketch() {
 
     override val sketch: SketchLoader by inject()
-    val audioProcessor: AudioProcessor by inject()
+    private val audioProcessor: AudioProcessor by inject()
+    private val galaxy: Galaxy by inject()
 
-    val movie = Movie(sketch, "movies/astral03_BACKGROUND.mp4")
-
-    override fun onBecameActive() {
+    private val movie1 = Movie(sketch, "movies/astral03_BACKGROUND.mp4")
+    private val movie2 = Movie(sketch, "movies/astral04_BACKGROUND.mp4")
+    private val astralLogo = sketch.loadImage("images/astrallogo_clean.png").apply {
+        resizeRatioAware(height = sketch.shorterDimension() / 2)
     }
 
+    private val movies = arrayOf(movie1, movie2)
+    private var activeMovie = movies[0]
+    private var nextSwitch = 0
+    private var logoVisible = false
+
+    private val videoPushButtons = galaxy.createPushButtonGroup(9, listOf(0, 1)) {
+        activeMovie = movies[it]
+    }
+
+    private val autoButton = galaxy.createToggleButton(9, 2, false)
+    private val logoButton = galaxy.createToggleButton(9, 3, false)
+
+    override fun onBecameActive() = Unit
+
     override fun setup() {
-        movie.loop()
+        movies.forEach { it.loop() }
     }
 
     override fun draw() = with(sketch) {
         background(0)
-        if (movie.available()) {
-            movie.read()
+
+        if (millis() > nextSwitch) {
+            nextSwitch = millis() + 15000
+            if (autoButton.isPressed) {
+                activeMovie = movies.random()
+            }
+
+            if (autoButton.isPressed && logoButton.isPressed) {
+                logoVisible = Random.nextBoolean()
+            }
         }
 
-        translate(centerX(), centerY())
+        if (autoButton.isPressed.not()) {
+            logoVisible = logoButton.isPressed
+        }
+
+        if (activeMovie.available()) {
+            activeMovie.read()
+        }
+
+        pushMatrix()
+        translateCenter()
         scale(1 + audioProcessor.getRange(20f..100f) / 4000f)
-        sketch.image(movie, -centerX(), -centerY(), width.toFloat(), height.toFloat())
+        sketch.image(activeMovie, -centerX(), -centerY(), width.toFloat(), height.toFloat())
+        popMatrix()
+
+        if (logoVisible) {
+            pushMatrix()
+            translateCenter()
+            scale(1 + audioProcessor.getRange(100f..200f) / 2000f)
+            sketch.image(
+                    astralLogo,
+                    -astralLogo.width / 2f,
+                    -astralLogo.height / 2f,
+                    astralLogo.width.toFloat(),
+                    astralLogo.height.toFloat()
+            )
+            popMatrix()
+        }
     }
 }
