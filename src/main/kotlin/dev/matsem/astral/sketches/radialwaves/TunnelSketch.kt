a4b33bb9a49b02abe73191595b2bc4d496576e83
matsem <matej.semancik@gmail.com>
2019-11-11 22:16:57 +0100

    Draw polygon

--- src/main/kotlin/dev/matsem/astral/sketches/radialwaves/TunnelSketch.kt
+++ src/main/kotlin/dev/matsem/astral/sketches/radialwaves/TunnelSketch.kt
@@ -3,10 +3,16 @@ package dev.matsem.astral.sketches.radialwaves
 import dev.matsem.astral.sketches.BaseSketch
 import dev.matsem.astral.sketches.SketchLoader
 import dev.matsem.astral.tools.audio.AudioProcessor
-import dev.matsem.astral.tools.extensions.*
+import dev.matsem.astral.tools.extensions.centerX
+import dev.matsem.astral.tools.extensions.centerY
+import dev.matsem.astral.tools.extensions.midiRange
+import dev.matsem.astral.tools.extensions.shorterDimension
 import dev.matsem.astral.tools.kontrol.KontrolF1
 import org.koin.core.inject
-import processing.core.PApplet
+import processing.core.PApplet.cos
+import processing.core.PApplet.sin
+import processing.core.PConstants.CLOSE
+import processing.core.PConstants.TWO_PI
 
 class TunnelSketch : BaseSketch() {
 
@@ -14,11 +20,32 @@ class TunnelSketch : BaseSketch() {
     private val kontrol: KontrolF1 by inject()
     private val audioProcessor: AudioProcessor by inject()
 
-    override fun onBecameActive() = with(sketch) {
-        ellipseMode(PApplet.CENTER)
-    }
+    override fun onBecameActive() = Unit
 
     override fun setup() = Unit
 
-    override fun draw() = Unit
+    override fun draw() = with(sketch) {
+        val strkWeight = kontrol.knob1.midiRange(1f, 20f)
+        val numPoints = kontrol.knob2.midiRange(3f, 12f).toInt()
+
+        background(bgColor)
+        noFill()
+        stroke(fgColor)
+
+        translate(centerX(), centerY(), 0f)
+        polygon(shorterDimension() / 2f, numPoints)
+    }
+
+    private fun polygon(radius: Float, nPoints: Int) = with(sketch) {
+        val angle = TWO_PI / nPoints
+        beginShape()
+        var a = 0f
+        while (a < TWO_PI) {
+            val sx = cos(a) * radius
+            val sy = sin(a) * radius
+            vertex(sx, sy)
+            a += angle
+        }
+        endShape(CLOSE)
+    }
 }
