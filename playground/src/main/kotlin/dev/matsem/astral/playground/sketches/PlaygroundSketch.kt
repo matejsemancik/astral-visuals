79c1b636b84aa4aaa2e4677d119752be028638eb
matsem <matej.semancik@gmail.com>
2020-07-29 20:56:04 +0200

    SEM logo experiment

--- playground/src/main/kotlin/dev/matsem/astral/playground/sketches/PlaygroundSketch.kt
+++ playground/src/main/kotlin/dev/matsem/astral/playground/sketches/PlaygroundSketch.kt
@@ -1,56 +1,81 @@
 package dev.matsem.astral.playground.sketches
 
+import ch.bildspur.postfx.builder.PostFX
 import dev.matsem.astral.core.tools.extensions.colorModeHsb
-import dev.matsem.astral.core.tools.extensions.drawShape
+import dev.matsem.astral.core.tools.extensions.pushPop
+import dev.matsem.astral.core.tools.extensions.withAlpha
 import dev.matsem.astral.core.tools.kontrol.KontrolF1
+import dev.matsem.astral.core.tools.shapes.ExtrusionCache
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import processing.core.PApplet
 import processing.core.PConstants
 import processing.core.PVector
+import processing.event.MouseEvent
 
 /**
  * Thing from http://piterpasma.nl/articles/rotating
  */
 class PlaygroundSketch : PApplet(), KoinComponent {
 
-    companion object {
-        const val N = 4096
-    }
-
     private val kontrol: KontrolF1 by inject()
+    private val ec: ExtrusionCache by inject()
+    private lateinit var fx: PostFX
 
-    fun circle(phi: Float, r: Float) = PVector(r * cos(phi), r * sin(phi))
-    fun R(f: Float, p: Float, a: Float, s: Float) = circle((s * f + p) * PConstants.TAU, a)
-    fun O(f: Float, p: Float, v: Float, d: Float, s: Float) = v + d * sin((s * f + p) * PConstants.TAU)
-
-    fun f(s: Float, t: Float) = PVector.add(
-        R(5f, O(7f, 0f, 0.25f, .2f, .3f), .7f, s),
-        R(-2f, 0f, O(7f, t, 1f, .5f, s), s)
-    )
+    val numX = 4
+    val numY = 4
 
     override fun settings() {
-        size(720, 720, PConstants.P2D)
+        size(720, 720, PConstants.P3D)
     }
 
     override fun setup() {
+        fx = PostFX(this)
         colorModeHsb()
         kontrol.connect()
+        ortho()
+        noLoop()
     }
 
     override fun draw() {
-        val t = millis() * 0.0002f
-        background(100f, 0f, 10f)
-        stroke(0f, 0f, 100f)
-        noFill()
-        strokeWeight(2f)
+        val bgColor = 0x0f0f0f.withAlpha()
+//        val fgColor = 0xebab34.withAlpha()
+        val fgColor = 0xffffff.withAlpha()
+        background(bgColor)
+        fill(bgColor)
+        stroke(fgColor)
+        strokeWeight(random(2f, 3f))
 
-        drawShape(PConstants.CLOSE) {
-            for (i in 0 until N) {
-                val s = i.toFloat() / N
-                val p = f(s, t)
-                vertex(width / 2f + 100 * p.x, width / 2f + 100 * p.y)
+        for (x in 0 until width step width / numX) {
+            for (y in 0 until height step height / numY) {
+                pushPop {
+                    val randomOffsetX = random(-1f, 1f)
+                    val randomOffsetY = random(-1f, 1f)
+                    translate(
+                        x.toFloat() + width / (numX * 2) + randomOffsetX,
+                        y.toFloat() + height / (numY * 2 + randomOffsetY)
+                    )
+                    scale(random(0.6f, 0.65f))
+                    rotate(random(PConstants.PI * 2f), random(1f), random(1f), random(1f))
+                    for (shape in ec.semLogo) {
+                        shape.disableStyle()
+                        shape(shape)
+                    }
+                }
             }
+        }
+
+        fx.render()
+            .bloom(0.5f, 20, 40f)
+            .rgbSplit(random(50f))
+            .compose()
+
+//        saveFrame("data/output/semlogo-###.png")
+    }
+
+    override fun mouseClicked(event: MouseEvent?) {
+        when(event?.button) {
+            PConstants.LEFT -> redraw()
         }
     }
 }
