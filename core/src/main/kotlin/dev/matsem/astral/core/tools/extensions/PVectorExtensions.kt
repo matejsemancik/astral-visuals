d6d5d8448c4c674e81065df3536a2a52aa6f5b1f
matsem <matej.semancik@gmail.com>
2020-08-05 22:23:41 +0200

    Reorder PVectorExtensions.kt functions

--- core/src/main/kotlin/dev/matsem/astral/core/tools/extensions/PVectorExtensions.kt
+++ core/src/main/kotlin/dev/matsem/astral/core/tools/extensions/PVectorExtensions.kt
@@ -4,14 +4,9 @@ import org.jbox2d.common.Vec2
 import processing.core.PVector
 import kotlin.math.absoluteValue
 
-// Destructing declaration for PVector class
 operator fun PVector.component1() = x
 operator fun PVector.component2() = y
 operator fun PVector.component3() = z
-
-fun PVector.toVec2() = Vec2(x, y)
-
-fun PVector.isInRadius(threshold: Float): Boolean = x.absoluteValue <= threshold && y.absoluteValue <= threshold
 
 operator fun PVector.plusAssign(anotherVector: PVector) {
     add(anotherVector)
@@ -20,3 +15,7 @@ operator fun PVector.plusAssign(anotherVector: PVector) {
 operator fun PVector.minusAssign(anotherVector: PVector) {
     sub(anotherVector)
 }
+
+fun PVector.toVec2() = Vec2(x, y)
+
+fun PVector.isInRadius(threshold: Float): Boolean = x.absoluteValue <= threshold && y.absoluteValue <= threshold
