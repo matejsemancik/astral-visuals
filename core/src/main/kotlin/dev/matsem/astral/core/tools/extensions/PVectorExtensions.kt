2d7580c1f9ac48f25f5096f812f37d1c295772f1
matsem <matej.semancik@gmail.com>
2020-08-05 22:22:55 +0200

    Add PVector destructing declaration operators

--- core/src/main/kotlin/dev/matsem/astral/core/tools/extensions/PVectorExtensions.kt
+++ core/src/main/kotlin/dev/matsem/astral/core/tools/extensions/PVectorExtensions.kt
@@ -4,6 +4,11 @@ import org.jbox2d.common.Vec2
 import processing.core.PVector
 import kotlin.math.absoluteValue
 
+// Destructing declaration for PVector class
+operator fun PVector.component1() = x
+operator fun PVector.component2() = y
+operator fun PVector.component3() = z
+
 fun PVector.toVec2() = Vec2(x, y)
 
 fun PVector.isInRadius(threshold: Float): Boolean = x.absoluteValue <= threshold && y.absoluteValue <= threshold
