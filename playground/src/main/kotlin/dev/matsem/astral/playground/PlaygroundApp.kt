1e619aa913be7413ba1bbb648c22bba13ae7fc08
matsem <matej.semancik@gmail.com>
2020-08-23 22:13:41 +0200

    Add metaballs playground

--- playground/src/main/kotlin/dev/matsem/astral/playground/PlaygroundApp.kt
+++ playground/src/main/kotlin/dev/matsem/astral/playground/PlaygroundApp.kt
@@ -1,7 +1,7 @@
 package dev.matsem.astral.playground
 
 import dev.matsem.astral.core.di.coreModule
-import dev.matsem.astral.playground.sketches.PixelSortingSketch
+import dev.matsem.astral.playground.sketches.MetaballsSketch
 import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
 import org.koin.core.inject
@@ -27,7 +27,7 @@ class PlaygroundApp : KoinComponent {
     fun run(processingArgs: Array<String>) {
         startKoin {
             printLogger(Level.ERROR)
-            modules(coreModule + playgroundModule { PixelSortingSketch() })
+            modules(coreModule + playgroundModule { MetaballsSketch() })
         }
 
         PApplet.runSketch(processingArgs + arrayOf("ProcessingPlayground"), sketch)
