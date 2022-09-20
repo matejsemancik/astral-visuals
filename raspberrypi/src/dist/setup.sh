ae565ba003a48e434cd34f280ce841a30185444c
matsem <matej.semancik@gmail.com>
2022-09-20 23:14:32 +0200

    Pimp up the NeonLogo sketch, add lineup for upcoming party

--- raspberrypi/src/dist/setup.sh
+++ raspberrypi/src/dist/setup.sh
@@ -3,3 +3,4 @@
 mkdir lib/shader
 mv lib/*.glsl lib/shader/
 mv images/ bin/images
+mv fonts/ bin/fonts
--- raspberrypi/src/main/kotlin/dev/matsem/astral/raspberrypi/sketches/NeonLogo.kt
+++ raspberrypi/src/main/kotlin/dev/matsem/astral/raspberrypi/sketches/NeonLogo.kt
@@ -1,22 +1,33 @@
 package dev.matsem.astral.raspberrypi.sketches
 
 import ch.bildspur.postfx.builder.PostFX
+import dev.matsem.astral.core.Files
 import dev.matsem.astral.core.tools.animations.AnimationHandler
 import dev.matsem.astral.core.tools.animations.radianSeconds
 import dev.matsem.astral.core.tools.extensions.colorModeHsb
+import dev.matsem.astral.core.tools.extensions.draw
+import dev.matsem.astral.core.tools.extensions.heightF
 import dev.matsem.astral.core.tools.extensions.pushPop
-import dev.matsem.astral.core.tools.extensions.quantize
 import dev.matsem.astral.core.tools.extensions.shorterDimension
+import dev.matsem.astral.core.tools.extensions.translate
 import dev.matsem.astral.core.tools.extensions.translateCenter
+import dev.matsem.astral.core.tools.extensions.widthF
 import dev.matsem.astral.core.tools.extensions.withAlpha
 import extruder.extruder
 import geomerative.RG
 import geomerative.RShape
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import processing.core.PApplet
 import processing.core.PConstants
+import processing.core.PFont
+import processing.core.PGraphics
 import processing.core.PShape
+import processing.core.PVector
 
 /**
  * Uses geomerative library to convert the futuredlogo.svg into 2D shape. Extruder library is then used to extrude the
@@ -27,16 +38,31 @@ class NeonLogo : PApplet(), AnimationHandler, KoinComponent {
     override fun provideMillis(): Int = millis()
 
     override fun settings() {
-        fullScreen(PConstants.P3D)
+//        fullScreen(PConstants.P3D)
+        size(1024, 768, PConstants.P3D)
     }
 
     private lateinit var fx: PostFX
     private val ex: extruder by inject()
+    private lateinit var font: PFont
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
-    private val logoToScreenScale = 0.4f
+    private val logoToScreenScale = 0.35f
     private val shapeDepth = 100
     private var sclOff = 0f
     private var rotYOff = 0f
+    private val starCount = 3000
+    private val fps = 15f
+    private lateinit var starsCanvas: PGraphics
+    private lateinit var stars: List<PVector>
+
+    private val logoPosition = PVector(0f, 0f)
+    private val logoPositionTarget = PVector(0f, 0f)
+    private val textPosition = PVector(0f, 0f)
+    private val textPositionTarget = PVector(0f, 0f)
+    private var lerpSpeed = 0.2f
+
+    private val mainColor = 0x00ffc8.withAlpha()
 
     data class Chunk(
         val originalShape: RShape,
@@ -51,12 +77,13 @@ class NeonLogo : PApplet(), AnimationHandler, KoinComponent {
         surface.setTitle("Futured")
         surface.setResizable(true)
         surface.hideCursor()
+        frameRate(fps)
 
         RG.init(this)
         RG.setPolygonizer(RG.UNIFORMSTEP)
         RG.setPolygonizerStep(1f)
 
-        chunks = listOf("images/semlogo_vector_top.svg", "images/semlogo_vector_bot.svg")
+        chunks = listOf("images/semlogo_top.svg", "images/semlogo_bottom.svg")
             .map { uri -> RG.loadShape(uri) }
             .map { rshape -> RG.polygonize(rshape) }
             .map { rshape ->
@@ -85,43 +112,128 @@ class NeonLogo : PApplet(), AnimationHandler, KoinComponent {
                 )
             }
 
+        starsCanvas = createGraphics(width, height, PConstants.P3D)
+        stars = generateSequence {
+            PVector(
+                random(-width.toFloat(), width.toFloat()),
+                random(-width.toFloat(), width.toFloat()),
+                random(-width.toFloat(), width.toFloat())
+            )
+        }.take(starCount).toList()
+
+        font = createFont(Files.Font.JETBRAINS_MONO, height / 20f, false)
         fx = PostFX(this)
+
+        coroutineScope.launch {
+            while (isActive) {
+                logoPositionTarget.set(0f, 0f)
+                textPositionTarget.set(widthF, 0f)
+                kotlinx.coroutines.delay(30_000L)
+
+                logoPositionTarget.set(-widthF * 0.4f, 0f)
+                textPositionTarget.set(-widthF * 0.2f, 0f)
+                kotlinx.coroutines.delay(30_000L)
+            }
+        }
     }
 
     override fun draw() {
-        ortho()
         background(0)
-        strokeWeight(6f)
-        stroke(0x00ffc8.withAlpha())
-        when {
-            mouseX.toFloat() in (0f..width / 3f) -> noFill()
-            mouseX.toFloat() in (width / 3f..width / 3f * 2f) -> fill(0xffffff.withAlpha(0))
-            else -> fill(0)
+
+        // Update props
+
+        stars.forEach {
+            it.z += 2f
+            if (it.z > width) {
+                it.z = random(-width.toFloat(), 0f)
+            }
         }
 
-        if (random(0f, 10f).quantize(0.1f) == 0f) {
-            sclOff = random(-0.1f, 0.1f)
-            rotYOff = random(-PI * .2f, PI * .2f)
-        }
+        logoPosition.lerp(logoPositionTarget, lerpSpeed)
+        textPosition.lerp(textPositionTarget, lerpSpeed)
 
-        translateCenter()
-        for (chunk in chunks) {
+        // endregion
+
+        // region Starfield
+
+        starsCanvas.draw {
+            fill(0x000000.withAlpha(32))
+            rect(0f, 0f, widthF, heightF)
+
             pushPop {
-                scale(
-                    width / chunk.shapeWidth * logoToScreenScale + sclOff,
-                    width / chunk.shapeWidth * logoToScreenScale + sclOff
-                )
+                noStroke()
+                fill(mainColor.withAlpha(128))
+                translateCenter()
 
-                rotateX(-PI * 0.1f)
-                rotateY(radianSeconds(30f) + rotYOff)
-                chunk.extrudedShape.forEach {
-                    shape(it)
+                stars.forEach {
+                    pushPop {
+                        translate(it.x, it.y, it.z)
+                        circle(0f, 0f, 3f)
+                    }
                 }
             }
         }
 
+        pushPop {
+            translate(0f, 0f, -400f)
+            image(starsCanvas, 0f, 0f)
+        }
+
+        // endregion
+
+        ortho()
+
+        // region Logo
+
+        pushPop {
+            translateCenter()
+            translate(logoPosition)
+            strokeWeight(10f)
+            stroke(0)
+            fill(mainColor)
+
+            for (chunk in chunks) {
+                pushPop {
+                    scale(
+                        width / chunk.shapeWidth * logoToScreenScale + sclOff,
+                        width / chunk.shapeWidth * logoToScreenScale + sclOff
+                    )
+
+                    rotateX(PI * 0.1f * sin(radianSeconds(60f)))
+                    rotateY(-radianSeconds(30f) + rotYOff)
+                    chunk.extrudedShape.forEach {
+                        shape(it)
+                    }
+                }
+            }
+        }
+
+        // endregion
+
+        // region Info text
+
+        val text = """
+            07:00pm matsem
+            08:30pm attempt
+            10:00pm SEBA
+            11:30pm NEED FOR MIRRORS
+            01:00am sbu & rough:result
+        """.trimIndent()
+
+        pushPop {
+            translateCenter()
+            translate(textPosition)
+            textFont(font)
+            textAlign(LEFT, CENTER)
+            noStroke()
+            fill(mainColor)
+            text(text, 0f, 0f)
+        }
+
+        // endregion
+
         fx.render().apply {
-            noise(0.2f, 0.1f)
+            noise(0.3f, 0.1f)
             pixelate(shorterDimension() / 2.4f)
         }.compose()
     }
